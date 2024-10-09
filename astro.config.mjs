a5e98ba9c09740410ce14f68eceb2cc29a5c1138
Fernando del Río <fernando.figueira@bootcamp-upgrade.com>
2024-10-09 12:37:21 +0200

    project changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://fernandodelriofigueira.github.io',
-  base: 'portfolio',
+  site: 'https://fernandodelriofigueira.com',
   trailingSlash: "never"
 })
